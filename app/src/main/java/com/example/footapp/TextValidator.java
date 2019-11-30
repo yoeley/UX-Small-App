28d4839c5dd0e4c8df9a722f1dd17b822b5a47a0
AviadSar <aviad.sar-shalom@mail.huji.ac.il>
2019-12-01 00:35:50 +0200

    adds the sending of JSONString of the new game from teamCreation to editTeam

--- app/src/main/java/com/example/footapp/TextValidator.java
+++ app/src/main/java/com/example/footapp/TextValidator.java
@@ -21,12 +21,32 @@ location.addTextChangedListener(new TextValidator(location) {
 
 public abstract class TextValidator implements TextWatcher {
     private final TextView textView;
+    private final int validation;
 
-    public TextValidator(TextView textView) {
+
+    final static public int numPlayersValid = 0;
+
+
+    final static private int minNumOfPlayers = 6;
+    final static private int maxNumOfPlayers = 6;
+    final static private String numOfPlayersMsg = "Number of players may only contain numbers";
+    final static private String numOfPlayersMinMaxMsg = "Number of players must be between " + minNumOfPlayers + " and " + maxNumOfPlayers;
+    final static private String numOfPlayersIsMsg = "Number of players must be " + minNumOfPlayers;
+
+    public TextValidator(TextView textView, int validation) {
         this.textView = textView;
+        this.validation = validation;
     }
 
-    public abstract void validate(TextView textView, String text);
+    private void validate(TextView textView, String text)
+    {
+        switch(validation)
+        {
+            case(numPlayersValid):
+                validateNumPlayers(textView, text);
+                break;
+        }
+    }
 
     @Override
     final public void afterTextChanged(Editable s) {
@@ -39,4 +59,41 @@ public abstract class TextValidator implements TextWatcher {
 
     @Override
     final public void onTextChanged(CharSequence s, int start, int before, int count) { /* Don't care */ }
+
+    private void validateNumPlayers(TextView textView, String text)
+    {
+        String number_regex = "\\d*";
+        // may be relevant for other validations
+        //Pattern p = Pattern.compile( "[^0-9]" );
+        //Matcher m = p.matcher(text);
+        //if (m.find()) {}
+        if(!text.matches(number_regex))
+        {
+            textView.setError(numOfPlayersMsg);
+        }
+        else
+        {
+            int input;
+            try
+            {
+                input = Integer.parseInt(text);
+            }
+            catch (NumberFormatException e)
+            {
+                textView.setError(numOfPlayersMsg);
+                return;
+            }
+            if(input < minNumOfPlayers || input > maxNumOfPlayers)
+            {
+                if(minNumOfPlayers == maxNumOfPlayers)
+                {
+                    textView.setError(numOfPlayersIsMsg);
+                }
+                else
+                {
+                    textView.setError(numOfPlayersMinMaxMsg);
+                }
+            }
+        }
+    }
 }
