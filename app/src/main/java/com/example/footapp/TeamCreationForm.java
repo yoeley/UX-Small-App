1f035bbb6572bb2b8d9b3320334ce251e4629b8c
AviadSar <aviad.sar-shalom@mail.huji.ac.il>
2019-12-02 16:01:13 +0200

    fixes the JSONString to match format. adds images from noa.

--- app/src/main/java/com/example/footapp/TeamCreationForm.java
+++ app/src/main/java/com/example/footapp/TeamCreationForm.java
@@ -53,13 +53,85 @@ public class TeamCreationForm extends AppCompatActivity {
     private void createGameJSON() {
         game = new JSONObject();
         try {
-            game.put("numOfPlayers", numOfPlayers.getText().toString());
             game.put("date", date.getText().toString());
             game.put("time", time.getText().toString());
             game.put("location", location.getText().toString());
-            game.put("captain", captain.getText().toString());
             game.put("judge", judge.getText().toString());
-            game.put("players", new JSONArray());
+
+            int numPlayers;
+            try {
+                numPlayers = Integer.parseInt(numOfPlayers.getText().toString());
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                numPlayers = 0;
+            }
+
+            JSONArray players1 = new JSONArray();
+            for (int i = 0; i < numPlayers; ++i)
+            {
+                JSONObject player = new JSONObject("{\"playerName\": \"\", \"playerId\": null, \"playerPos\": \"\"}");
+                players1.put(player);
+            }
+            JSONArray players2 = new JSONArray();
+            for (int i = 0; i < numPlayers; ++i)
+            {
+                JSONObject player = new JSONObject("{\"playerName\": \"\", \"playerId\": null, \"playerPos\": \"\"}");
+                players2.put(player);
+            }
+
+
+            JSONArray teams = new JSONArray();
+
+            JSONObject team1 = new JSONObject();
+            team1.put("teamName", "teamOne");
+            team1.put("numOfPlayers", numOfPlayers.getText().toString());
+            team1.put("captain", captain.getText().toString());
+            team1.put("players", players1);
+
+            JSONObject team2 = new JSONObject();
+            team2.put("teamName", "teamTwo");
+            team2.put("numOfPlayers", numOfPlayers.getText().toString());
+            team2.put("captain", captain.getText().toString());
+            team2.put("players", players2);
+
+            teams.put(team1);
+            teams.put(team2);
+
+            game.put("teams", teams);
+
+
+            /*
+            {
+                "date": "12.12.2019",
+                "time": "18:00",
+                "location": "Jerusalem",
+                "judge": "Sam",
+                "teams": [
+                            {
+                            "teamName": "teamOne",
+                            "numOfPlayers": "6",
+                            "captain": "Ain?",
+                            "players":
+                                [
+                                {"playerName": "Yoel", "playerId": 0, "playerPos": "GK"},
+                                {"playerName": "Dan", "playerId": 1, "playerPos": "MID"}
+                                ]
+                            },
+                            {
+                            "teamName": "teamTwo",
+                            "numOfPlayers": "6",
+                            "captain": "Capt",
+                            "players":
+                                [
+                                {"playerName": "Avi", "playerId":2, "playerPos":"GK"},
+                                {"playerName": "Mar", "playerId":3, "playerPos":"MID"},
+                                {"playerName": "Noa", "playerId":4, "playerPos":"LF"}
+                                ]
+                            }
+                ]
+}
+             */
         }
         catch (org.json.JSONException e) {
             e.printStackTrace();
@@ -77,13 +149,13 @@ public class TeamCreationForm extends AppCompatActivity {
         EditTeam.putExtra("Game", game.toString());
 
         //TODO: delete this printing option before submission
-        /*
+
         try {
             System.out.println(game.toString(4));
         }
         catch (org.json.JSONException e) {
             e.printStackTrace();
-        }*/
+        }
         startActivity(EditTeam);
     }
     public void toggle_contents(View v){
