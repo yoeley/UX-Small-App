f6be6a18fdb75e364c7cf65e51381e69d9f57b57
AviadSar <aviad.sar-shalom@mail.huji.ac.il>
2019-12-09 03:44:44 +0200

    adds validation of all fields + validation of empty fields + verify the name is unique

--- app/src/main/java/com/example/footapp/TextValidator.java
+++ app/src/main/java/com/example/footapp/TextValidator.java
@@ -4,37 +4,52 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.TextView;
 
+import java.util.List;
+
 public abstract class TextValidator implements TextWatcher {
     private final TextView textView;
     private final int validation;
+    private List<String> gamesNames;
+    private TeamCreationForm teamCreationForm;
 
-
-    final static public int numPlayersValid = 0;
-
+    final static public int emptyChecker = 0;
+    final static public int gameNameValid = 1;
+    final static public int numPlayersValid = 2;
 
     final static private int minNumOfPlayers = 6;
     final static private int maxNumOfPlayers = 6;
     final static private String numOfPlayersMsg = "Number of players may only contain numbers";
     final static private String numOfPlayersMinMaxMsg = "Number of players must be between " + minNumOfPlayers + " and " + maxNumOfPlayers;
     final static private String numOfPlayersIsMsg = "Number of players in beta must be " + minNumOfPlayers;
+    final static private String gameNameMsg = "A game named %s already exists. choose a unique name";
+    final static private String emptyFieldMsg = "Required field";
 
-    public TextValidator(TextView textView, int validation) {
+    public TextValidator(TextView textView, int validation, TeamCreationForm teamCreationForm) {
         this.textView = textView;
         this.validation = validation;
+        this.teamCreationForm = teamCreationForm;
+        this.gamesNames = this.teamCreationForm.getGamesNames();
     }
 
     private void validate(TextView textView, String text)
     {
+        validateEmptyField(textView, text);
+
         switch(validation)
         {
             case(numPlayersValid):
                 validateNumPlayers(textView, text);
+                break;
+
+            case(gameNameValid):
+                validateGameName(textView, text);
                 break;
         }
     }
 
     @Override
     final public void afterTextChanged(Editable s) {
+        teamCreationForm.setCreateButtonActiveOrNot();
         String text = textView.getText().toString();
         validate(textView, text);
     }
@@ -75,6 +90,26 @@ public abstract class TextValidator implements TextWatcher {
                     textView.setError(numOfPlayersMinMaxMsg);
                 }
             }
+        }
+    }
+
+    private void validateGameName(TextView textView, String text)
+    {
+        if (text.equals("")) {
+            return;
+        }
+        for (int i = 0; i < gamesNames.size(); ++i) {
+            if (text.equals(gamesNames.get(i)))
+            {
+                textView.setError(String.format(gameNameMsg, text));
+            }
+        }
+    }
+
+    private void validateEmptyField(TextView textView, String text)
+    {
+        if (text.trim().equals("")) {
+            textView.setError(String.format(emptyFieldMsg));
         }
     }
 }
